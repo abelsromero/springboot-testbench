605fd05d7d0d58de9dc84fd3e48ebb18d3d16ed0
Abel Salgado Romero <abelromero@gmail.com>
2021-10-10 11:32:02 +0200

    Implemented CLI native Hello World

--- cli/src/main/java/com/example/testbench/Starter.java
+++ cli/src/main/java/com/example/testbench/Starter.java
@@ -2,7 +2,9 @@ package com.example.testbench;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Starter implements ApplicationRunner {
 	
 	@Override
